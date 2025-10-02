e24dcccb507c1b34af106ffa241f37bba7a17c8e
Gabriel0Ramos0 <gabrielrcorrea@outlook.com>
2025-10-01 21:12:55 -0300

    resolvendo atividade 1

--- 1a-aula/main.py
+++ 1a-aula/main.py
@@ -1,1 +1,36 @@
-print("Hello Word!")
+print ("Olá, mundo!")
+print ("----------------------")
+
+nome = input("Qual é o seu nome? ")
+print(nome)
+print ("----------------------")
+
+numero = 7
+numero2 = 3
+print(numero + numero2)
+print ("----------------------")
+
+nome = input("Qual é o seu nome? ")
+print ("Olá, " + nome + "!")
+print ("----------------------")
+
+valor = "Gabriel"
+print ("Olá, " + valor + "!")
+valor = "Foguinho"
+print ("Olá, " + valor + "!")
+print ("----------------------")
+
+curso = "Python"
+print ("Estou aprendendo <" + curso + ">")
+print ("----------------------")
+
+nota1 = 9
+nota2 = 7
+print ("A soma é de: " + str(nota1 + nota2))
+print ("----------------------")
+
+nome = "Gabriel"
+idade = 22
+cidade = "Orleans"
+print ("Meu nome é " + nome + ", tenho " + str(idade) + " anos e moro em " + cidade + ".")
+print ("----------------------")
