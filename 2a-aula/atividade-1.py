9f91e30ca01628ff36823d732a2638ab4502b351
Gabriel0Ramos0 <gabrielrcorrea@outlook.com>
2025-10-23 08:07:41 -0300

    feito algumas correções na atividade 1

--- 2a-aula/atividade-1.py
+++ 2a-aula/atividade-1.py
@@ -1,51 +1,56 @@
+from datetime import date, time, datetime
+
 # Atividade 1
 
 n1 = 21
-n2 = float(10)
+n2 = 10.5
 n3 = True
 n4 = "Python"
 n5 = None
-n6 = bytes ("Olá".encode('utf-8'))
+n6 = bytes("Olá".encode('utf-8'))
 n7 = bytearray('Olá', 'utf-8')
 
 print(n1, n2, n3, n4, n5, n6, n7, sep="\n")
 print("#----------------------#")
-print (type(n1), type(n2), type(n3), type(n4), type(n5), type(n6), type(n7), sep="\n")
+print(type(n1), type(n2), type(n3), type(n4),
+      type(n5), type(n6), type(n7), sep="\n")
 print("#----#------#-----#----#")
 
-# Atividade 2
+# Atividade 2 - Mutável
 
-lista = [ 30, 5, 10, 20, 50 ]
-print ("Tamanho da lista:", len(lista))
-print ("Lista em Ordem Crecente:", sorted(lista))
-print ("lista em Ordem Decrescente:", sorted(lista, reverse=True))
+lista = [30, 5, 10, 20, 50]
+print("Tamanho da lista:", len(lista))
+print("Lista em Ordem Crecente:", sorted(lista))
+print("lista em Ordem Decrescente:", sorted(lista, reverse=True))
 print("#----#------#-----#----#")
 
-# Atividade 3
+# Atividade 3 - Imutável
 
 cores = ("Azul", "Vermelho", "Verde")
-print (cores[0])
-print (cores.count( "Vermelho" ))
-print (cores.index( "Verde" ))
+print(cores[0])
+print(cores.count("Vermelho"))
+print(cores.index("Verde"))
 print("#----#------#-----#----#")
 
 # Atividade 4
 
-lista2 = set( [ 1, 2, 3, 4, 5, 4, 2 ] )
-print (lista2)
+lista2 = set([1, 2, 3, 4, 5, 4, 2])
+print(lista2)
 print("#----#------#-----#----#")
 
 # Atividade 5
 
-fixa = {1: "nome", 2: "idade", 3: "curso"}
-print (fixa[1])
-print (fixa.get(2))
-print (fixa.get(4, "Chave 4 não encontrada"))
+fixa = {
+    "nome": "Gabriel",
+    "idade": 22,
+    "curso": "Sistemas de Informação"
+}
+print("Nome:", fixa["nome"])
+print("Idade:", fixa["idade"])
+print("Curso:", fixa["curso"])
 print("#----#------#-----#----#")
 
 # Atividade 6
-
-from datetime import date, time, datetime
 
 date = datetime.now()
 
